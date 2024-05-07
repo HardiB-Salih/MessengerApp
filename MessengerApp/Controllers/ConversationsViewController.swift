79b533fd87dea8e1f303627382cb03e5df0ead9e
HardiB.Salih <30411248+HardiB-Salih@users.noreply.github.com>
2024-05-07 15:22:14 +0300

    Wrap up getting started

--- MessengerApp/Controllers/ConversationsViewController.swift
+++ MessengerApp/Controllers/ConversationsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ConversationsViewController.swift
 //  MessengerApp
 //
 //  Created by HardiB.Salih on 5/7/24.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
 }
-
